c58d7be99099019056d87e1de60fe798c624383e
bhavan <bhavanl@techprovint.com>
2022-11-17 14:15:33 +0530

    Boilerplate

--- src/App.js
+++ src/App.js
@@ -1,68 +1,10 @@
-import React, { useEffect, useState } from 'react';
-
-import Result from './components/Result';
-import QuizBox from './components/QuizBox';
-import Loading from './components/Loading';
+import React from 'react';
 
 import './style.css';
 
 export default function App() {
-  const [questions, setQuestions] = useState([]);
-  const [score, setScore] = useState(0);
-  const [showResults, setShowResults] = useState(false);
-  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-
-  const getData = async () => {
-    const data = await fetch('data.json');
-    const response = await data.json();
-    setQuestions(response.questions);
-  };
-
-  useEffect(() => {
-    getData();
-  }, []);
-
-  const handleAnswerOption = (currentQuestion, answer) => {
-    if (currentQuestion.answers[currentQuestion.correctAnswer - 1] === answer) {
-      setScore(score + 1);
-    }
-
-    const nextQuestionIndex = currentQuestionIndex + 1;
-
-    if (nextQuestionIndex < questions.length) {
-      setCurrentQuestionIndex(nextQuestionIndex);
-    } else {
-      setShowResults(true);
-    }
-  };
-
-  const handleRestart = () => {
-    setScore(0);
-    setCurrentQuestionIndex(0);
-    setShowResults(false);
-  };
-
   return (
-    <div className="app">
-      {showResults ? (
-        <Result
-          score={score}
-          totalQuestions={questions.length}
-          onRestart={handleRestart}
-        />
-      ) : (
-        <>
-          {questions.length > 0 ? (
-            <QuizBox
-              questions={questions}
-              currentQuestionIndex={currentQuestionIndex}
-              onAnswerClick={handleAnswerOption}
-            />
-          ) : (
-            <Loading />
-          )}
-        </>
-      )}
+    <div className="app">  
     </div>
   );
 }
--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,18 +1,8 @@
 import React from 'react';
 
-function Result({ score, totalQuestions, onRestart }) {
-  return (
-    <>
-      <div className="score-section">
-        <h3>
-          You scored {score} out of {totalQuestions}
-        </h3>
-        <div>
-          <button onClick={() => onRestart()}>Restart</button>
-        </div>
-      </div>
-    </>
-  );
+function Result() {
+    return (<></>)
+
 }
 
 export default Result;
